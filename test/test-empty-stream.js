36d0f66903bb519263470dea9f7c8a34f74deff5
Florent Jaby <fjaby@octo.com>
2013-07-05 11:58:55 +0200

    test with empty array

--- test/test-empty-stream.js
+++ test/test-empty-stream.js
@@ -1,4 +1,6 @@
 var sink = require('stream-sink');
+var domain = require('domain');
+
 var parser = require('../');
 var stream = require('stream');
 
@@ -14,3 +16,24 @@ exports['Test empty stream'] = function (test) {
   p.on('error', test.fail.bind(test, "No error should occur"));
   source.end();
 }
+
+exports['Test stream with empty array'] = function (test) {
+  var source = new stream.PassThrough();
+  var p = parser();
+  var d = domain.create();
+  d.on('error', function(err) {
+    console.error(err.stack);
+    test.fail('no error should occur but got', err.message);
+    test.done();
+  });
+
+  d.run(function () {
+    source.pipe(p).pipe(sink({objectMode: true})).on('data', function(data) {
+      test.equals(0, data.length, 'there should be no data at the end');
+      test.done();
+    });
+    p.on('data', test.fail.bind(test, "There shoud be no data emitted"));
+    source.end('[]');
+  })
+}
+
