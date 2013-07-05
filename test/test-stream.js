ab1178ebf2186f80cdc57dd98bc75772c194a679
Florent Jaby <fjaby@octo.com>
2013-07-05 16:31:22 +0200

    test for invalid json

--- test/test-stream.js
+++ test/test-stream.js
@@ -58,6 +58,22 @@ exports['Test stream with non closing array'] = function (test) {
   source.end('[ ');
 }.withDomain()
 
+exports['Invalid array [,]'] = function (test) {
+  var source = new stream.PassThrough();
+  var p = parser();
+
+  source.pipe(p).pipe(sink({objectMode: true})).on('data', function(data) {
+    test.equals(0, data.length, 'there should be no data at the end');
+  });
+  p.on('data', test.fail.bind(test, "There shoud be no data emitted"));
+  p.on('error', function(e) {
+    test.equal('Unexpected token ,', e.message, "An error should be raised");
+    test.done();
+  });
+  source.end('[,]');
+}.withDomain()
+
+
 exports['test with array with one number in it'] = function (test) {
   var source = new stream.PassThrough();
   var p = parser();
@@ -100,4 +116,36 @@ exports['test with array with 2 strings in it'] = function (test) {
     test.equal(line, a.shift(), 'We should get our number in the data event');
   });
   source.end(JSON.stringify(a));
+}.withDomain()
+
+exports['Array of objects'] = function (test) {
+  var a = [{object: "one"}, {object: 2}, {"last object": true}];
+  var source = new stream.PassThrough();
+  var p = parser();
+  test.expect(4);
+  source.pipe(p).pipe(sink({objectMode: true})).on('data', function(data) {
+    test.equal(3, data.length, 'There should be some data at the end');
+    test.done();
+  });
+  p.on('data', function(line) {
+    test.equal(JSON.stringify(line), JSON.stringify(a.shift()), 'We should get our number in the data event');
+  });
+  source.end(JSON.stringify(a));
+}.withDomain()
+
+
+exports['Array of objects with missing comma'] = function (test) {
+  var source = new stream.PassThrough();
+  var p = parser();
+  test.expect(1);
+  source.pipe(p).pipe(sink({objectMode: true})).on('data', function(data) {
+    test.fail('End should never get called');
+    test.done();
+  });
+  p.on('data', test.fail.bind(test, "There shoud be no data emitted"));
+  p.on('error', function(e) {
+    test.equal('Unexpected token {', e.message, "An error should be raised");
+    test.done();
+  });
+  source.end('[ {"object": 1} {"o":"three"}]');
 }.withDomain()
