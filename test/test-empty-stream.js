67386f1726bd01791e62d0a9657c7d8b74296351
Florent Jaby <fjaby@octo.com>
2013-07-05 12:12:46 +0200

    handle non complete arrays

--- test/test-empty-stream.js
+++ test/test-empty-stream.js
@@ -37,3 +37,25 @@ exports['Test stream with empty array'] = function (test) {
   })
 }
 
+exports['Test stream with non closing array'] = function (test) {
+  var source = new stream.PassThrough();
+  var p = parser();
+  var d = domain.create();
+  d.on('error', function(err) {
+    console.error(err.stack);
+    test.fail('no error should occur but got', err.message);
+    test.done();
+  });
+
+  d.run(function () {
+    source.pipe(p).pipe(sink({objectMode: true})).on('data', function(data) {
+      test.equals(0, data.length, 'there should be no data at the end');
+    });
+    p.on('data', test.fail.bind(test, "There shoud be no data emitted"));
+    p.on('error', function(e) {
+      test.equal('Unexpected end of input', e.message, "An error should be raised");
+      test.done();
+    });
+    source.end('[ ');
+  })
+}
